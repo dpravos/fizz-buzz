3f096e2d2bc8ceae7f0e24fcd8e07216ac7ee991
David Pravos <dpravos@gmail.com>
2021-06-18 00:14:00 +0200

    generalize fizzbuzz

--- src/main/java/com/dpravos/fizzbuzz/FizzBuzz.java
+++ src/main/java/com/dpravos/fizzbuzz/FizzBuzz.java
@@ -2,15 +2,7 @@ package com.dpravos.fizzbuzz;
 
 public class FizzBuzz {
     public String value(int number) {
-        if (number == 15) {
-            return "FizzBuzz";
-        }
-
-        if (number == 30) {
-            return "FizzBuzz";
-        }
-
-        if (number == 45) {
+        if (number % 15 == 0) {
             return "FizzBuzz";
         }
 
