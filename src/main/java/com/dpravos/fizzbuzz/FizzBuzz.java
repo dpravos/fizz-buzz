853440ea66fc78cddac72e2c6bfa21c70d8f304f
David Pravos <dpravos@gmail.com>
2021-06-17 23:09:52 +0200

    generalize not multiple of 3 nor 5 case

--- src/main/java/com/dpravos/fizzbuzz/FizzBuzz.java
+++ src/main/java/com/dpravos/fizzbuzz/FizzBuzz.java
@@ -2,14 +2,6 @@ package com.dpravos.fizzbuzz;
 
 public class FizzBuzz {
     public String value(int number) {
-        if (number == 4) {
-            return "4";
-        }
-
-        if (number == 2) {
-            return "2";
-        }
-
-        return "1";
+        return String.valueOf(number);
     }
 }
