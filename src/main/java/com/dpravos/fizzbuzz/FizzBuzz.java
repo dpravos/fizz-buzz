dd4f33e024b76716125f6ac960d524a2ef0ff87f
David Pravos <dpravos@gmail.com>
2021-06-17 23:35:52 +0200

    merge sequential IFs

--- src/main/java/com/dpravos/fizzbuzz/FizzBuzz.java
+++ src/main/java/com/dpravos/fizzbuzz/FizzBuzz.java
@@ -2,15 +2,7 @@ package com.dpravos.fizzbuzz;
 
 public class FizzBuzz {
     public String value(int number) {
-        if (number == 3) {
-            return "Fizz";
-        }
-
-        if (number == 6) {
-            return "Fizz";
-        }
-
-        if (number == 9) {
+        if (number == 3 || number == 6 || number == 9) {
             return "Fizz";
         }
 
